b5d9c93647061ea2113a140134ef25efe76ca1a0
renzycode <renzyjohnm1@gmail.com>
2022-07-31 00:20:58 +0800

    fix

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
     <!-- New Added -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="assets\css\style2.css">
+    <link rel="stylesheet" href="assets/css/style2.css">
 </head>
 
 <body>
