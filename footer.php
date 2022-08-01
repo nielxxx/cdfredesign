e07ee783fb59032cd9f0bfcc4ab9d1dd9211d2bd
renzycode <renzyjohnm1@gmail.com>
2022-08-01 13:52:49 +0800

    commited

--- footer.php
+++ footer.php
@@ -13,39 +13,25 @@
                     </p>
                 </div>
                 <div class="col-lg-3 col-md-6 footer-links">
-                    <h4>Useful Links</h4>
-                    <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a href="index.php#hero">Home</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="index.php#about">About</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="index.php#services">Services</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="product.php">Products</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 footer-links">
                     <h4>Our Social Networks</h4>
                     <p>Visit our social media to learn more about us!</p>
                     <div class="social-links mt-3">
-                        <!--
-                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            -->
                         <a href="https://www.facebook.com/CDFIMPORTS/?ref=pages_you_manage" class="facebook"><i
                                 class="bx bxl-facebook"></i></a>
                         <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                        <!--
-                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                            -->
+                    </div>
+                    <br>
+                    <h4>Disclaimer</h4>
+                    <div class="social-links">
+                        <a href="#" class="instagram"><i class="bi bi-exclamation-diamond-fill"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container footer-bottom clearfix">
+    <div class="container footer-bottom clearfix ">
         <div class="copyright">
-            &copy; CDF TRADING <strong><span></span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-            Designed by <a href="#">LGC IT Team</a>
+            &copy; Copyright . All Rights Reserved . CDF TRADING . <span class="footer-cred"> <a href="http://">LGC IT Team </a></span>
         </div>
     </div>
 </footer>
